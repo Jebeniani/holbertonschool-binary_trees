145b7281beb0712ccc880aca45a964f1d0aa3770
jebeniani <aymenjebeniani05@gmail.com>
2022-07-20 07:20:01 +0100

    new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - function that creates a node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node = NULL;
+
+    new_node = malloc(siezof(binary_tree_t));
+    if (!new_node)
+        return (NULL);
+    new_node->n = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    new_node->parent = parent;
+    return (new_node);
+}
