10bd32174f3a7051c2132b9722b9905fb96cd31c
jebeniani <aymenjebeniani05@gmail.com>
2022-07-20 10:17:31 +0100

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,14 +10,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     binary_tree_t *new;
 
     new = malloc(siezof(binary_tree_t));
-
     if (new == NULL)
         return (NULL);
 
     new->n = value;
+    new->parent = parent;
     new->left = NULL;
     new->right = NULL;
-    new->parent = parent;
 
     return (new);
 }
