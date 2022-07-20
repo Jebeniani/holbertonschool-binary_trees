39e92b6af1926272e9254fc8e515a1e1c13062c6
jebeniani <aymenjebeniani05@gmail.com>
2022-07-20 10:09:48 -0700

    Deforestation

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,15 +2,15 @@
 /**
  * binary_tree_delete - deletes an entire binary tree
  * @tree: pointer to the root node of the tree to delete
- * 
+ *
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-        if (!tree)
-            return;
+	if (!tree)
+		return;
 
-        binary_delete_tree(tree->left);
-        binary_delete_tree(tree->right);
+	binary_delete_tree(tree->left);
+	binary_delete_tree(tree->right);
 
-        free(tree);
+	free(tree);
 }
