0bce9aeebc288d0da89da21f859ce4b8ab40417e
jebeniani <aymenjebeniani05@gmail.com>
2022-07-20 07:27:53 +0100

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,9 +9,10 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = NULL;
+    binary_tree_t *new_node;
 
     new_node = malloc(siezof(binary_tree_t));
+    
     if (!new_node)
         return (NULL);
     new_node->n = value;
