f78d9dbb997c9e5483b5650eed56f2fd664c9b0b
jebeniani <aymenjebeniani05@gmail.com>
2022-07-20 10:33:15 -0700

    Deforestation

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,8 +9,8 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (!tree)
 		return;
 
-	binary_delete_tree(tree->left);
-	binary_delete_tree(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
 	free(tree);
 }
